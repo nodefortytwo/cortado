3950552caf7ad7e679a1f9d013fe58d5316c5a5e
Rick Burgess <github@rickburgess.me>
2018-03-29 22:15:48 +0100

    basic stuff

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func main() {
+	// Initialize a session in us-west-2 that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials.
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1")},
+	)
+
+	// Create S3 service client
+	svc := s3.New(sess)
+
+	bucket := os.Args[1]
+
+	// Get the list of items
+	resp, err := svc.ListObjects(&s3.ListObjectsInput{Bucket: aws.String(bucket)})
+	if err != nil {
+		exitErrorf("Unable to list items in bucket %q, %v", bucket, err)
+	}
+
+	for _, item := range resp.Contents {
+		fmt.Println("Name:         ", *item.Key)
+		fmt.Println("Last modified:", *item.LastModified)
+		fmt.Println("Size:         ", *item.Size)
+		fmt.Println("Storage class:", *item.StorageClass)
+		fmt.Println("")
+	}
+
+	fmt.Println("Found", len(resp.Contents), "items in bucket", bucket)
+	fmt.Println("")
+
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
+
+func editFile(fpath string) {
+	f, err := os.Create(fpath)
+	if err != nil {
+		log.Printf("1")
+		log.Fatal(err)
+	}
+	f.Close()
+
+	cmd := exec.Command("vim", fpath)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Start()
+	if err != nil {
+		log.Printf("2")
+		log.Fatal(err)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		log.Printf("Error while editing. Error: %v\n", err)
+	} else {
+		log.Printf("Successfully edited.")
+	}
+}
