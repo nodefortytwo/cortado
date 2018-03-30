8c4e3f9aa7aa56a482d6620f9a45be44c24724c4
Rick Burgess <github@rickburgess.me>
2018-03-30 10:30:01 +0100

    technically working!

--- main.go
+++ main.go
@@ -7,13 +7,14 @@ import (
 	"os/exec"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func main() {
-	// Initialize a session in us-west-2 that the SDK will use to load
+	// Initialize a session in eu-west-1 that the SDK will use to load
 	// credentials from the shared credentials file ~/.aws/credentials.
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("eu-west-1")},
@@ -23,53 +24,20 @@ func main() {
 	svc := s3.New(sess)
 
 	bucket := os.Args[1]
+	prefix := os.Args[2]
 
 	// Get the list of items
-	resp, err := svc.ListObjects(&s3.ListObjectsInput{Bucket: aws.String(bucket)})
+	resp, err := svc.ListObjects(&s3.ListObjectsInput{Bucket: aws.String(bucket), Prefix: aws.String(prefix)})
 	if err != nil {
 		exitErrorf("Unable to list items in bucket %q, %v", bucket, err)
 	}
 
+	key := *resp.Contents[0].Key
+
 	fpath := os.TempDir() + "cortado"
-	fmt.Println(fpath)
-	file, err := os.Create(fpath)
-	if err != nil {
-		exitErrorf("Unable to open file %q, %v", err)
-	}
-
-	downloader := s3manager.NewDownloader(sess)
-	item := *resp.Contents[0].Key
-
-	numBytes, err := downloader.Download(file,
-		&s3.GetObjectInput{
-			Bucket: aws.String(bucket),
-			Key:    aws.String(item),
-		})
-	if err != nil {
-		exitErrorf("Unable to download item %q, %v", item, err)
-	}
-
-	file.Close()
-
-	fmt.Println("downloaded", numBytes)
-	fmt.Println("")
-
+	downloadFile(bucket, key, fpath, sess)
 	editFile(fpath)
-
-	file, err = os.Open(fpath)
-
-	uploader := s3manager.NewUploader(sess)
-
-	_, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(item),
-		Body:   file,
-	})
-	if err != nil {
-		exitErrorf("Unable to upload %q to %q, %v", item, bucket, err)
-	}
-
-	fmt.Printf("Successfully uploaded %q to %q\n", item, bucket)
+	uploadFile(bucket, key, fpath, sess)
 
 }
 
@@ -96,4 +64,41 @@ func editFile(fpath string) {
 	} else {
 		log.Printf("Successfully edited.")
 	}
+}
+
+func downloadFile(bucket string, key string, fpath string, sess client.ConfigProvider) {
+	file, err := os.Create(fpath)
+	if err != nil {
+		exitErrorf("Unable to open file %q, %v", err)
+	}
+
+	downloader := s3manager.NewDownloader(sess)
+
+	_, err = downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(key),
+		})
+	if err != nil {
+		exitErrorf("Unable to download item %q, %v", key, err)
+	}
+
+	file.Close()
+}
+
+func uploadFile(bucket string, key string, fpath string, sess client.ConfigProvider) {
+	file, err := os.Open(fpath)
+
+	uploader := s3manager.NewUploader(sess)
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   file,
+	})
+	if err != nil {
+		exitErrorf("Unable to upload %q to %q, %v", key, bucket, err)
+	}
+
+	fmt.Printf("Successfully uploaded %q to %q\n", key, bucket)
 }
